6b3cca78b90275fd117b23894d7985633f513191
Larry Parba <larz.parba@gmail.com>
2020-12-16 14:25:40 +0400

    add

--- resources/views/clients/pages/presentations.blade.php
+++ resources/views/clients/pages/presentations.blade.php
@@ -69,6 +69,20 @@
             </div>
         </div>
 
+
+        <div class="col-md-3">
+          <div class="card">
+              <img class="card-img-top" src="https://banner2.cleanpng.com/20180531/rtf/kisspng-pdf-computer-icons-information-rpse-5b0fbe63918ea4.4536074215277584355962.jpg" alt="Card image cap">
+              <div class="card-body">
+              <h5 class="card-title">November - SEO Report</h5>
+              {{-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> --}}
+              <a target="_blank" href="{{ asset('uploads/presentations/Noralign -  SEO Report - November.pdf') }}" class="btn btn-primary">View Presentation</a>
+              </div>
+          </div>
+      </div>
+
+        
+
         </div>
 
 
